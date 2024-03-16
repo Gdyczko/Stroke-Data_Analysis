c04ace13a1b44c992035320dea474a881ec73e63
Grzegorz Dyczko <117686941+Gdyczko@users.noreply.github.com>
2024-03-16 21:54:05 +0100

    Update RStudio.R
    english version

--- RStudio.R
+++ RStudio.R
@@ -88,16 +88,16 @@ summary(dane$gender)
 # Creating a vector with binary data that needs to be changed
 zmienneBinarne <- c("hypertension", "heart_disease", "stroke")
 
-# podgląd podusmowanych danych - trzech na raz
-lapply(dane[, zmienneBinarne], function(x){summary(factor(x))}) # wygląda ok, więc zastosujmy
+# Previewing the subset of data - three at a time
+lapply(dane[, zmienneBinarne], function(x){summary(factor(x))}) # Looks good, so let's apply it
 
-# zamiana danych hypertension, heart_disease i stroke na factor
+# Converting data in hypertension, heart_disease, and stroke columns to factors
 dane[, zmienneBinarne] <- lapply(dane[, zmienneBinarne], factor,
                                  levels = c(0, 1),
                                  labels = c("No", "Yes")
                                  )
 
-# sprwwdzenie czy przekształcenie przebiegło w sposób prawidłowy
+# Checking if the transformation has been carried out correctly
 summary(dane[, zmienneBinarne])
 
 str(dane) # podpatrzenie struktury co jest jeszcze do zmiany 
@@ -135,53 +135,44 @@ dane$smoking_status <- factor(dane$smoking_status,
                             )
 summary(dane$smoking_status)
 
-# sprawdzenie ponownie struktury czy jakieś dane zostały do przetworzenia
+# Checking the data structure again to see if any data is left for processing
 str(dane)
 
 
-#######################
-# Analiza statystyczna
-#######################
+# Statistical analysis
 
-
-# Obliczenie dla wieku kwantylu na poziomie 25% i 75% z próby
+# Calculating the 25% and 75% quantiles of age from the sample
 quantile(dane$age, c(p=0.25, p=0.75))
 
-# najmłodszy wiekiem z badanych osób
+# Youngest age among the participants
 min(dane$age)
 
-# najstarszy wiekiem z badanych osób
+# Oldest age among the participants
 max(dane$age)
 
-# średnia wieku u badanych
+# Mean age of the participants
 mean(dane$age)
 
-# obliczenie odchylenia standardowego 
+# Calculating the standard deviation
 sd(dane$age)
 
-# obliczenie skośności
-install.packages("moments") # instalacja pakietu moments
-library(moments) # załadowanie pakietu 
-skewness(dane$age) # skośność
+# Calculating skewness
+install.packages("moments") # Installing the moments package
+library(moments) # Loading the package 
+skewness(dane$age) # Skewness
 
-############################################################################################
-# Z przeprowadzonych analiz ststystycznych można wywnioskować, że w badaniach brały udział #  
-# zarówno dzieci jak i osoby starsze. Najmłodzsze dziecko miało niecały miesiąc (0,08),    #
-# natomiast najstarsza badana osoba miała 82 lata. Średnia wieku dla badanej grópy wynosi  #
-# około 43 lata. Odchylenie standardowe mówiące o rozrzucie danych od wartości średniej    #
-# wynosi 22.61358. Zaobserwowano również, że rozkład analizowanych danych dotyczących      #
-# wieku przyjmuje lekką asymetrię lewostronną (około -0,14), co oznacza, że w naszej       #
-# badanej grópie jest trochę wiecej osób z wiekiem powyżej 43 roku życia. Podczas          #
-# przeprowadzonych badań, połowa respondentów (50%) była w wieku w przedziale 25 - 61 lat. #  
-############################################################################################
+# From the conducted statistical analysis, it can be inferred that both children and 
+# older individuals participated in the study. The youngest child was less than a month old (0.08), 
+# while the oldest participant was 82 years old. The average age for the study group is around 43 years. 
+# The standard deviation, indicating the spread of data from the mean value, is 22.61358. Additionally, 
+# it was observed that the distribution of analyzed age data exhibits slight left-skewness (approximately -0.14), 
+# suggesting that there are slightly more individuals above the age of 43 in our study group. During the conducted 
+# research, half of the respondents (50%) were aged between 25 and 61 years.
 
 
-######################
-# Wizualizacja danych
-######################
+# Data Visualization
 
-
-# instalacja dodatkowych pakeitów, które mogą nam się przydać podzczas prowadzonych analiz
+# Installing additional packages that may be useful during the analysis
 install.packages("ggplot2")
 install.packages("tidyr")
 install.packages("ggcorrplot")
@@ -195,8 +186,7 @@ library(viridis)
 library(corrplot)
 library(wesanderson)
 
-
-# wykres dotyczący rodzaju zatrudnienia
+# Barplot for employment type
 barplot(summary(dane$work_type),
         main = "Number of people by employment",
         ylim = c(0, 3000),
@@ -204,20 +194,19 @@ barplot(summary(dane$work_type),
         xlab = "Type of work",
         col = viridis(5))
 
-###################################################################################################
-# Wykres przedstawia liczbę badanych, w zależności od rodzaju zatrudnienia. W ramach pięciu       #
-# zadeklarowanych kategrii obok osób zatrudnionych w prywatnych przedsiębiorstwach (Private)      #
-# jak i jednostkach rządowych (Government job) oraz samozatrudninych (Self-employed) znalały się  #
-# osoby, które nigdy nie pracowały (Never worked) oraz grupa dzieci (Children).                   #
-# Z przeprowadzonej analizy wynika, że dominującą grupą w badaniu były osoby, które               #
-# deklarowały, iż pracują w przedsiębiorstwach prywatnych (blisko 3000 osób), kilkakrotnie        #
-# przewyższając liczebnie pozostałe kategorie. Grupa osób deklarujących pracę w jednostkach       #
-# rządowych jest bardzo zbliżona do liczby dzieci biorących udział w badaniu. Nieznacznie         #
-# liczniejszą grupę stanowią osoby pracujące we własnych firmach (samozatrudnione), jest nieco    #
-# ponad 800 osób. Natomiast najmniej liczącą grupę stanowiły osoby, kótre nigdny nie pracowały.   #
-###################################################################################################
+# The above barplot represents the number of respondents, depending on the type of employment.    
+# Within the five declared categories, besides people employed in private enterprises (Private),  
+# government jobs (Government job), and self-employed (Self-employed), there were also people who 
+# had never worked (Never worked) and a group of children.                                        
+# From the analysis, it follows that the dominant group in the study were people who declared that 
+# they work in private enterprises (close to 3000 people), several times more numerous than other  
+# categories. The group of people declaring work in government institutions is very similar in     
+# number to the number of children participating in the study. Slightly more numerous are the      
+# people working in their own companies (self-employed), with just over 800 people. The least      
+# numerous group consisted of people who had never worked.                                          
 
-# wykres dotyczący poziomu BMI według miejsca zmaieszkania
+
+# Boxplot for BMI level by place of residence
 boxplot(dane$bmi ~ dane$Residence_type,
         range = 0,
         main = "BMI level by place of residence",
@@ -234,18 +223,16 @@ legend("topright",
 abline(h=24.9, col="blue", lty = 5, lwd = 2)
 text(21.5, "MAX correct BMI", col = "blue", pos = 2, cex = 1.3)
 
-################################################################################################
-# Powyższy wykres przedstawia poziom indeksu BMI u badanych osób według                        #
-# miejsca zamieszkania. Dodatkowo na wykresie zaznaczono przerywaną niebieską linię            #
-# przedstawiającą maksymlany poziom prawidłowego BMI (MAX corect BMI) na poziomie 24.9.        #
-# Z przeprowadzonej analizy wynika, że większość osób (ok. 75%), niezaleźnie od miejsca        #
-# zamieszkania, ma poziom BMI powyżej normy. U ludzi mieszkających na wsi występuje większa    #
-# różnorodność wartości BMI (większy rozrzut danych o ok. 20 pkt) niż u ludzi mieszkających    #
-# w mieście. Połowa badanych osób, zarówno tych mieszkających na wsi jak i w mieście ma BMI na #
-# poziomie od ok. 24 do 33 punktów.                                                            #
-################################################################################################
+# The above plot shows the BMI index level in respondents according to their place of residence.
+# Additionally, a dashed blue line indicating the maximum correct BMI level (MAX corect BMI)   
+# at 24.9 is marked on the plot. From the analysis, it follows that most people (about 75%),    
+# regardless of their place of residence, have a BMI above the norm. Rural residents exhibit a  
+# greater diversity of BMI values (a larger spread of data by about 20 points) than urban       
+# residents. Half of the respondents, both rural and urban, have a BMI ranging from about 24    
+# to 33 points.                                                                                
 
-# wykres dotyczący poziomu BMI według typu pracy
+
+# Boxplot for BMI level by type of work
 boxplot(dane$bmi ~ dane$work_type,
         range = 0,
         main = "BMI level by type of work",
@@ -255,19 +242,19 @@ boxplot(dane$bmi ~ dane$work_type,
 abline(h=24.9, col="red", lty = 5, lwd = 2)
 text(28, "MAX correct BMI", col = "red", cex = 1.3)
 
-#################################################################################################
-# Wykres "BMI level by type of work" przedstawia zakres wartości BMI w zależoności od rodzaju   #
-# wykonywanej pracy. Również w tym wykresie jak i poprzednim naniesiono dodatkowo czerwoną      #
-# przerywaną linię, która wskazuje górną granicę prawidłowego poziomu BMI (MAX corect BMI).     #
-# Z analizowanego wykresu możemy wywnioskować, że większość badanych prezentuje wartość BMI     #
-# powyżej normy niezależnie od rodzaju ztrudnienia. Jedynie osoby nigdy nie pracujące w ok. 50% #
-# mieszczą się w granicach prawidłowego BMI. Wyjątkiem na wykresie jest grupa badanych dzieci,  #
-# które w znaczenj części (ponad 75% badanych) znajdują się poniżej górnej granicy prawidłowej  #
-# masy ciała. Grupa badanych, pracująca w sektorze prywatnym prezentuje najszerszy zakres       #
-# wartości BMI oraz znajdują się osoby z najwyższym wskaźniekiem BMI.                           #
-#################################################################################################
 
-# sprawdzenie czy występują silne korelacje pomiędzy którymiś danymi z psośród wszystkich danych
+# The plot "BMI level by type of work" presents the range of BMI values depending on the type   
+# of work performed. Also, in this plot as in the previous one, a dashed red line, which        
+# indicates the upper limit of the correct BMI level (MAX corect BMI), was marked. From the     
+# analyzed plot, it can be inferred that most respondents present a BMI value above the norm,   
+# regardless of the type of employment. Only people who have never worked are within the normal 
+# BMI range in about 50% of the cases. An exception on the plot is the group of children, who   
+# in a significant part (over 75% of respondents) have BMI below the upper limit of the normal  
+# body mass. The group of respondents working in the private sector presents the widest range   
+# of BMI values, and there are people with the highest BMI index.                               
+
+
+# Checking if there are strong correlations between any of the data among all the data
 model.matrix(~0+., data = dane) %>% 
   cor(use="pairwise.complete.obs") %>% 
   ggcorrplot(show.diag = FALSE,
@@ -276,20 +263,16 @@ model.matrix(~0+., data = dane) %>%
              lab_size=3,
              title = "Corelation between all data")
 
-############################################################################################
-# Wykres "Corelation between all data" prezentuje wyniki zależnośći pomiędzy wszytskimi    #
-# badanymi czynikami. Dzięki takiemu zabiegowi wykres sam wskazuje możliwe istneijące      #
-# korelacje pomiędzy poszczegónymi czynnikami. Z wykresu wynika, że pomiędzy badanymi      #
-# czynnikami praktycznie nie ma żadnej korelacji lub jest ona bardzo słaba, a w niektórych #
-# przypadkach umiarkowana. Wizualizacja głównie przyjmuje wartości dodatnie, ale wystęują  #
-# również wartości ujemne w przypadku osób, które nie podały statusu palenia. Ze względu   #
-# na fak iż badania zebrano w celu przeprowadzenia analiz dotyczących występowania udaru,  #
-# interpretując powyższy wykres możemy wywnioskować, że nie odnaleziono żadnych czynników  #
-# (np. palenie tytoniu, zawały serca, nadciśnienie), które miałby by istotny wpływ na      #
-# pojawienie się tej choroby. Korelacja z żadnym czynnikiem nie przekracza wartości 0,25.  #
-# Jedyna silna korelacja jak pojawia sie wizualizacji dotyczy wieku i bycia w związku      #
-# małżeńskim. Jest to korelacja dodatnia i wynosi 0,68. Oznacza to, że im osoba jest       #
-# starza tym większe prawdopodobieństo, że znajduje się lub znajdowała się w związku       #
-# małżeńskim.                                                                              #
-############################################################################################
-
+# The "Corelation between all data" plot presents the results of the correlations between  
+# all examined factors. Thanks to this approach, the plot itself indicates possible        
+# correlations between individual factors. From the plot, it can be seen that there is      
+# practically no correlation between the examined factors or it is very weak, and in some   
+# cases moderate. The visualization mainly takes positive values, but there are also        
+# negative values in the case of people who did not provide smoking status. Since the        
+# research was collected to conduct analyses regarding the occurrence of stroke, interpreting
+# the above plot, it can be inferred that no factors (e.g., smoking, heart attacks, high blood
+# pressure) were found to have a significant impact on the occurrence of this disease.       
+# Correlation with any factor does not exceed the value of 0.25. The only strong correlation
+# appearing in the visualization is between age and marital status. This is a positive      
+# correlation and it is 0.68. It means that the older the person, the higher the probability
+# that they are or have been in a marital relationship.                                      
